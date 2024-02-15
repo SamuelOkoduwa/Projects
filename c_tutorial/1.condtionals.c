2b0b8771caed72f9378b1619fa903fee6eec482a
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-02-15 15:30:29 +0100

    Conditions in c

---
+++ c_tutorial/1.condtionals.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// int main(void) {
+// 	int x = 20;
+// 	int y = 30;
+// 	if (x > y) {
+// 		printf("c is greater than y");
+// 	}
+// 	else
+// 	{
+// 		printf("c is less than y");
+// 	}
+// 	return 0;
+// }
+
+// Loops 
+int main(void) {
+	// int i = 0;
+	// while (i < 5) {
+	// 	printf("%d\n", i);
+	// 	i++;
+	// }
+
+	int i = 0;
+	do {
+		printf("%i\n", i);
+		i++;
+	}
+	while (i < 5);
+}
